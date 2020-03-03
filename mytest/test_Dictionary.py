73e968a7bde2b9aa3b4f768551de39538570d3a2
fuckinggirl <lada.laikina@yandex.ru>
2020-03-04 00:48:43 +0300

    after linter

--- mytest/test_Dictionary.py
+++ mytest/test_Dictionary.py
@@ -9,27 +9,28 @@ class TestForDictionary:
     # )
     # @pytest.mark.parametrize("x", [{8:'kk'},{'aa':8,'ss':7}])
     # @pytest.mark.parametrize("y", [{8:'kk'},{'aa':8,'ss':7}])
-    @pytest.mark.parametrize("x,y", [({8:'kk'},{8:'kk'}), ({'aa':8,'ss':7},{'aa':8,'ss':7})])
-    def test_copy(self,x,y):
+    @pytest.mark.parametrize("x,y", [({8: 'kk'}, {8: 'kk'}), ({'aa': 8, 'ss': 7}, {'aa': 8, 'ss': 7})])
+    def test_copy(self, x, y):
         # возвращает копию словаря
-        assert x==y.copy()
+        assert x == y.copy()
 
     @pytest.mark.parametrize("x,y", [({}, []), ({'aa': 8, 'ss': 7}, ['aa', 'ss'])])
-    def test_keys(self,x,y):
+    def test_keys(self, x, y):
         # возвращает объект ключей в словаре который должен переводиться в список
-        assert list(x.keys())==y
+        assert list(x.keys()) == y
 
     @pytest.mark.parametrize("x,y", [({}, None), ({'aa': 8, 'ss': 7}, None)])
-    def test_clean(self,x,y):
+    def test_clean(self, x, y):
         # очищает словарь
-        assert x.clear()==y
+        assert x.clear() == y
+
     @pytest.mark.parametrize("x,y", [({}, []), ({'aa': 8, 'ss': 7}, [8, 7])])
-    def test_values(self,x,y):
+    def test_values(self, x, y):
         # возвращает значения в словаре  который должен переводиться в список
         assert list(x.values()) == y
 
     @pytest.mark.parametrize("x,y", [({}, []), ({'aa': 8, 'ss': 7}, [('aa', 8), ('ss', 7)])])
-    def test_items(self,x,y):
+    def test_items(self, x, y):
         # возвращает пары (ключ, значение)
         assert list(x.items()) == y
         pass
--- mytest/test_Int.py
+++ mytest/test_Int.py
@@ -1,30 +1,32 @@
 import pytest
+
+
 class TestForInt:
-    @pytest.mark.parametrize("x,y", [(3,7), (0,9),(-1,2)])
-    def test_big(self,x,y):
+    @pytest.mark.parametrize("x,y", [(3, 7), (0, 9), (-1, 2)])
+    def test_big(self, x, y):
         # >
-        assert (y>x) ==True
+        assert (y > x) == True
 
     @pytest.mark.parametrize("x,y", [(3, 7), (0, 9), (-1, 2)])
-    def test_little(self,x,y):
+    def test_little(self, x, y):
         # <
         assert (x < y) == True
 
     @pytest.fixture()
     def some_data(self):
-        a=7
+        a = 7
         return a
 
-    def test_is(self,x=some_data,y=some_data):
+    def test_is(self, x=some_data, y=some_data):
         # one object
         assert (x is y) == True
 
     @pytest.mark.parametrize("x,y", [(3, 3), (0, 0), (-1, -1)])
-    def test_equial(self,x,y):
+    def test_equial(self, x, y):
         # ==
         assert (x == y) == True
 
     @pytest.mark.parametrize("x,y", [(3, 7), (0, 9), (-1, 2)])
-    def test_not_equial(self,x,y):
+    def test_not_equial(self, x, y):
         # !=
         assert (x != y) == True
